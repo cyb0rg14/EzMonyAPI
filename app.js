aebc436fd95e71fab71bde660a637830f9075035
datamaven14 <techtitanbot@gmail.com>
2023-11-01 03:18:53 +0530

    Auto commit

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const app = express();
 const PORT = process.env.PORT;
 
 // middlewares
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: '',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
