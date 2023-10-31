0396f3981928d1502dba371b36b7bdb2432a45f2
datamaven14 <techtitanbot@gmail.com>
2023-11-01 04:15:55 +0530

    Auto commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db.js");
 const authRoutes = require("./routes/authRoutes.js");
 const cors = require("cors");
+const middlewares = require("./middleware");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +12,8 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 // routes
 app.use("/api/v1/auth", authRoutes);
