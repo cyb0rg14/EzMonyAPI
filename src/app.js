b3f32924f5f4b6d8ee9880148579cd0403b2cf11
datamaven14 <techtitanbot@gmail.com>
2023-11-01 04:06:11 +0530

    Auto commit

--- src/app.js
+++ src/app.js
@@ -8,14 +8,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 // middlewares
-// app.use(cors());
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
