23c537f5c8e97696dbf44b55a4424ad3787f7315
datamaven14 <techtitanbot@gmail.com>
2023-11-01 01:40:45 +0530

    Auto commit

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 // routes
 app.use("/api/v1/auth", authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello World');  
+})
 const startServer = async () => {
   try {
     await connectDB();
