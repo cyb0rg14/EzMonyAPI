06e748e37672ba9337af0ae8e11549eec17f28e9
datamaven14 <techtitanbot@gmail.com>
2023-11-02 03:32:45 +0530

    Auto commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,7 +23,7 @@ export const getOTP = async (req, res) => {
       const newOTP = new OTP({ email, otp });
       await newOTP.save();
     }
-    await sendmail(AUTH_EMAIL, AUTH_PASS, email, otp);
+    sendmail(AUTH_EMAIL, AUTH_PASS, email, otp);
     res.status(200).send(`OTP successfully sent to ${email}`);
   } catch (error) {
     res.status(500).send(error.message);
