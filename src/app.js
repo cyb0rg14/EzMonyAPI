77249e9cc935977e56356fed11a4dd49d94dc517
datamaven14 <techtitanbot@gmail.com>
2023-11-02 00:32:06 +0530

    Auto commit

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Welcome to EzMony API!");
 });
 
 // routes
