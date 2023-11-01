ec0219e4864fd540226664de2d1d4e3fb60e75ec
datamaven14 <techtitanbot@gmail.com>
2023-11-02 00:28:24 +0530

    Auto commit

--- src/utils/sendmail.js
+++ src/utils/sendmail.js
@@ -1,39 +1,40 @@
-import nodemailer from "nodemailer" 
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import nodemailer from "nodemailer";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export function compileEmailContent(otp) {
-    const currentModuleURL = import.meta.url
-    const currentModulePath = fileURLToPath(currentModuleURL)
-    const htmlFilePath = path.join(currentModulePath, '..', 'otpmail.html')
-    const htmlTemplate = fs.readFileSync(htmlFilePath, 'utf8')
-    const compiledHTML = htmlTemplate.replace('${otp}', otp)
-    return compiledHTML
+function compileEmailContent(otp) {
+  const currentModuleURL = import.meta.url;
+  const currentModulePath = fileURLToPath(currentModuleURL);
+  const htmlFilePath = path.join(currentModulePath, "..", "otpmail.html");
+  const htmlTemplate = fs.readFileSync(htmlFilePath, "utf8");
+  const compiledHTML = htmlTemplate.replace("${otp}", otp);
+  return compiledHTML;
 }
 
-export const sendmail = (senderMail, senderPassword, receiverMail, otp) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: senderMail,
-            pass: senderPassword,
-        }
-    })
+const sendmail = (senderMail, senderPassword, receiverMail, otp) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: senderMail,
+      pass: senderPassword,
+    },
+  });
 
-    
-    const mailOptions = {
-        from: senderMail,
-        to: receiverMail,
-        subject: "EzMony Verification Code",
-        html: compileEmailContent(otp)
+  const mailOptions = {
+    from: senderMail,
+    to: receiverMail,
+    subject: "EzMony Verification Code",
+    html: compileEmailContent(otp),
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
+  });
+};
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error)
-        } else {
-            console.log('Email sent: ' + info.response)
-        }
-    })
-}
+export default sendmail;
